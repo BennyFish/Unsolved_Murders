7c0dc42803acba59b42fd49906fe3914b6c66787
Benjamin Fisher <b.fish000@gmail.com>
2018-06-23 10:06:21 -0400

    Working Site

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -9,21 +10,25 @@ engine = create_engine("sqlite:///Murder.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-fbiData = Base.classes.fbi_db
+# fbiData = Base.classes.fbi_db
 stateData = Base.classes.state_db
 
 session = Session(engine)
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     
-    return f'Landing Page'
+    return render_template('index.html')
+    
+@app.route('/datacleaning')
+def data_cleaning():
+    return render_template('data_cleaning.html')
 
-@app.route('/api/v1.0/fbidatabase')
-def fbi_api():
-    sel = [fbiData]
-    return jsonify(fbi_api)
+@app.route('/dataanalysis')
+def data_analysis():
+    return render_template('messing_with_data.html')
 
 @app.route('/api/v1.0/statedatabase')
 def state_api():
@@ -34,3 +39,10 @@ def state_api():
     state_data.append(state_dict)
     return jsonify (state_data)
 
+# @app.route('/api/v1.0/fbidatabase')
+# def fbi_api():
+#     sel = [fbiData]
+#     return jsonify(fbi_api)
+
+if __name__ == "__main__":
+    app.run(debug=True)
